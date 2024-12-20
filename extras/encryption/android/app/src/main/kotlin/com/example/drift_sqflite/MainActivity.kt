96b3947fc16de99ffe25bcabc124e3b3a7c69571
Simon Binder <oss@simonbinder.eu>
2024-12-20 21:00:15 +0100

    Update sqflite_sqlcipher encryption package

--- extras/encryption/android/app/src/main/kotlin/com/example/drift_sqflite/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.drift_sqflite
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ extras/encryption/android/app/src/main/kotlin/com/example/encrypted_drift/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.encrypted_drift
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
